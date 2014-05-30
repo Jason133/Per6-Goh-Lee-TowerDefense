123b2bb40abcc702fd61c2ed4b033ddce9f0108e
computergeek18 <stuyjasonlee97@gmail.com>
2014-05-29 21:50:40 -0400

    Minor Deletions
    - Deleted Tower, it’s not a tile
    - Edited Driver as appropriate

--- Driver.java
+++ Driver.java
@@ -1,16 +1,5 @@
 
 public class Driver {
-	
-	public static void map1 (Map a) {
-		for (int i = 0; i < a.getLength(); i++) {
-			FreeTower b = new FreeTower(i,0);
-			a.setMap(b);
-		}
-		for (int i = 0; i < a.getLength(); i++) {
-			Tower b = new Tower(i,1);
-			a.setMap(b);
-		}
-	}
 
 	public static void main(String[] args) {
 		Map a = new Map("BalancedMap.txt");
