a10428bdd900bc0d9299f60880bf447a5573cb92
computergeek18 <stuyjasonlee97@gmail.com>
2014-05-29 22:06:53 -0400

    Tower Class Updated
    - Just an implementation
    - All in comments of what to do

---
+++ Tower.java
@@ -0,0 +1,22 @@
+
+public class Tower {
+
+	// Will continue over weekend
+	// Each FREETOWER (tile) class will have its own tower (variable)
+	// it will initally be null
+	// if you add(map m, xcor,ycor) change the freetower appropriately
+	// We can just leave instance variables alone, like speed/etc.
+	// Just want to be able to insert it and have the tiles have 
+	// the freetower and hasTower on it (hasTower can be a method)
+	// Main thing is just to add and place
+	
+	// ENEMIES: 
+	// Maybe make the path a linked list? (for order of the movement) 
+	// They come second after placing tower
+	// maybe find way to make them move? 
+	
+	public Tower(int xcor, int ycor) {
+		
+	}
+
+}
